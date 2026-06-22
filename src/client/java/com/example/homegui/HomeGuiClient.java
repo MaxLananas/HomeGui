6dccbe185da83c1f17f8ce32444769ed145a28f1
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-06-22 23:39:51 +0000

    Update HomeGuiClient.java

--- src/client/java/com/example/homegui/HomeGuiClient.java
+++ src/client/java/com/example/homegui/HomeGuiClient.java
@@ -6,8 +6,8 @@ import com.example.homegui.screen.HomesScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.KeyMapping;
+import com.mojang.blaze3d.platform.InputConstants;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,27 +15,23 @@ import org.slf4j.LoggerFactory;
 public class HomeGuiClient implements ClientModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("homegui");
-    public static final String MOD_ID = "homegui";
 
-    private static KeyBinding openGuiKey;
+    private static KeyMapping openGuiKey;
 
     @Override
     public void onInitializeClient() {
-        // Charger config + langue
         ModConfig.getInstance();
         LangManager.getInstance().loadFromConfig();
 
-        // Enregistrement du keybind
-        openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
                 "key.homegui.open_gui",
-                InputUtil.Type.KEYSYM,
+                InputConstants.Type.KEYSYM,
                 GLFW.GLFW_KEY_H,
                 "category.homegui.main"
         ));
 
-        // Tick client
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (openGuiKey.wasPressed()) {
+            while (openGuiKey.consumeClick()) {
                 if (client.player != null) {
                     HomesManager.getInstance().requestHomes();
                     client.setScreen(new HomesScreen());
